706c65c2c47e227ad2f5bb484b3f7a1b1368ab9c
Anastassiya <nastya_po@list.ru>
2023-03-28 18:07:08 +0300

    описана фукнция map

--- functions.py
+++ functions.py
@@ -1,2 +1,5 @@
 def filter_query(value, data):
     return filter(lambda x: value in x, data)
+
+def map_query(value, data):
+    return map(lambda x: x.split()[value], data)
--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 
-from functions import filter_query
+from functions import *
 
 main_bp = Blueprint('main_bp', __name__)
 
@@ -8,8 +8,15 @@ main_bp = Blueprint('main_bp', __name__)
 @main_bp.route('/perform_query', methods=['POST'])
 def perform_query():
     data = request.json
+    # with open(data['file_name']) as f:
+    #     result = filter_query(
+    #         value = data['value1'],
+    #         data = f.readlines()
+    #     )
+    # return jsonify(list(result))
+
     with open(data['file_name']) as f:
-        result = filter_query(
+        result = map_query(
             value = data['value1'],
             data = f.readlines()
         )
