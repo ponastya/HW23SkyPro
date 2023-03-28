e2792913afb979b72297b70ca9f71a07aac867f2
Anastassiya <nastya_po@list.ru>
2023-03-28 17:35:04 +0300

    описана фукнция фильтрации

---
+++ functions.py
@@ -0,0 +1,2 @@
+def filter_query(value, data):
+    return filter(lambda x: value in x, data)
--- views.py
+++ views.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from functions import filter_query
 
 main_bp = Blueprint('main_bp', __name__)
 
 
-@main_bp.route('/perform_query', methods=['GET', 'POST'])
+@main_bp.route('/perform_query', methods=['POST'])
 def perform_query():
-    return f"Я страничкв профиля пользователя "
+    data = request.json
+    with open(data['file_name']) as f:
+        result = filter_query(
+            value = data['value1'],
+            data = f.readlines()
+        )
+    return jsonify(list(result))
